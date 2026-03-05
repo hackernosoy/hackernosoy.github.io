878b2c8fbdb57fcf4a668b56247835dd66132dda
hackernosoy <166301699+hackernosoy@users.noreply.github.com>
2026-03-05 15:32:23 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+let carrito = [];
+let total = 0;
+
+function agregar(nombre,precio){
+
+carrito.push(nombre + " $" + precio);
+total += precio;
+
+let lista = document.getElementById("lista");
+
+let item = document.createElement("li");
+item.textContent = nombre + " $" + precio;
+
+lista.appendChild(item);
+
+document.getElementById("total").textContent = total;
+
+}
+
+function enviar(){
+
+let mensaje = "Hola quiero comprar:%0A";
+
+carrito.forEach(p=>{
+mensaje += "- " + p + "%0A";
+});
+
+mensaje += "Total: $" + total;
+
+let numero = "549TU_NUMERO";
+
+window.open("https://wa.me/" + numero + "?text=" + mensaje);
+
+}
