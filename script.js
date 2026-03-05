60aad183e9a26861e081bbea6086b08863980b05
hackernosoy <166301699+hackernosoy@users.noreply.github.com>
2026-03-05 15:40:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 let carrito = [];
 let total = 0;
 
-function agregar(nombre,precio){
+function agregar(nombre, precio){
 
-carrito.push(nombre + " $" + precio);
-total += precio;
+carrito.push({nombre, precio});
+actualizarCarrito();
+
+}
+
+function eliminar(index){
+
+total -= carrito[index].precio;
+carrito.splice(index,1);
+
+actualizarCarrito();
+
+}
+
+function actualizarCarrito(){
 
 let lista = document.getElementById("lista");
+lista.innerHTML = "";
+
+total = 0;
+
+carrito.forEach((producto,index)=>{
+
+total += producto.precio;
 
 let item = document.createElement("li");
-item.textContent = nombre + " $" + precio;
+
+item.innerHTML =
+producto.nombre + " $" + producto.precio +
+" <button onclick='eliminar("+index+")'>❌</button>";
 
 lista.appendChild(item);
+
+});
 
 document.getElementById("total").textContent = total;
 
@@ -22,12 +47,12 @@ function enviar(){
 let mensaje = "Hola quiero comprar:%0A";
 
 carrito.forEach(p=>{
-mensaje += "- " + p + "%0A";
+mensaje += "- " + p.nombre + " $" + p.precio + "%0A";
 });
 
 mensaje += "Total: $" + total;
 
-let numero = "5492212231836";
+let numero = "549TU_NUMERO";
 
 window.open("https://wa.me/" + numero + "?text=" + mensaje);
 
